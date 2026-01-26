e75eeec4c450ab89b69f73dd03a66f859e372745
madhu <dharmaraj.madhumitha@gmail.com>
2026-01-26 16:16:04 -0600

    Add health check endpoint

---
+++ src/test/java/com/madhu/fitmind/FitmindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madhu.fitmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitmindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
